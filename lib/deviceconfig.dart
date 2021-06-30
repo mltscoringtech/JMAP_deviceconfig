a6afca7c9d411234311e84044b91ac9a3fe1fe00
mltscoringtech <michael@scoringtech.com>
2021-06-30 09:03:25 -0600

    Update deviceconfig.dart

--- lib/deviceconfig.dart
+++ lib/deviceconfig.dart
@@ -1,8 +1,9 @@
 import 'package:JMAP_deviceconfig/controllers/wifiscan.dart';
 import 'package:JMAP_deviceconfig/widgets/navdrawer.dart';
-import 'package:JMAP_deviceconfig/widgets/progress_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+var ssidData;
 
 class DeviceConfigPage extends StatefulWidget {
   const DeviceConfigPage({Key key}) : super(key: key);
@@ -14,6 +15,7 @@ class DeviceConfigPage extends StatefulWidget {
 
 class _DeviceConfigPageState extends State<DeviceConfigPage> {
   WiFiScanGetData _wiFiScan = WiFiScanGetData();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,9 +44,80 @@ class _DeviceConfigPageState extends State<DeviceConfigPage> {
               leading: Icon(Icons.adjust_sharp),
               title: Text("Step 4: Select the SSID to join."),
             ),
-
             Center(child: ProgressButton()),
+            Text("ssidData: ${ssidData ?? "None"} "),
           ],
         ));
   }
+}
+
+class ProgressButton extends StatefulWidget {
+  const ProgressButton({Key key}) : super(key: key);
+
+  @override
+  _ProgressButtonState createState() => _ProgressButtonState();
+}
+
+class _ProgressButtonState extends State<ProgressButton> with TickerProviderStateMixin {
+  WiFiScanGetData _wiFiScan = WiFiScanGetData();
+  int _state = 0;
+  double _width = 240;
+  GlobalKey _globalKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      key: _globalKey,
+      width: _width,
+      height: 48,
+      child: ElevatedButton(
+        style: raisedButtonStyle,
+        onPressed: () async {
+          setState(() {
+            animateButton();
+          });
+          ssidData = await _wiFiScan.getSSID().then((value) {
+            setState(() {
+              animateButton();
+              ssidData = value;
+            });
+          });
+        },
+        child: searchButtonContent(),
+      ),
+    );
+  }
+
+  void animateButton() {
+    var animationController = AnimationController(duration: Duration(milliseconds: 300), vsync: this);
+    Animation _animation = Tween(begin: 0.0, end: 1.0).animate(animationController)
+      ..addListener(() {
+        setState(() {
+          _width = (_state == 1) ? 48 : 240;
+        });
+      });
+    animationController.forward();
+
+    setState(() {
+      _state = (_state == 0) ? 1 : 0;
+    });
+  }
+
+  Widget searchButtonContent() {
+    if (_state == 0) {
+      return Text("Scan for SSID's");
+    } else {
+      return CircularProgressIndicator(
+        value: null,
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+      );
+    }
+  }
+
+  final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+      primary: Colors.red,
+      padding: EdgeInsets.all(0),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+      ));
 }
